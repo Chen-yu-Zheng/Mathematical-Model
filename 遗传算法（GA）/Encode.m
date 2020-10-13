35ed49b4bb032ae390d38b38bd128286d0a10f79
Chenyu Zheng <61927614+Chen-yu-Zheng@users.noreply.github.com>
2020-10-13 11:47:34 +0800

    Update Encode.m

--- 遗传算法（GA）/Encode.m
+++ 遗传算法（GA）/Encode.m
@@ -1,5 +1,4 @@
 function [binpool] = Encode(pool,Length,Minimum,Maximum)
     pool = (pool - Minimum)*(2^Length - 1) / (Maximum - Minimum);
-    binpool = dec2bin(pool);
-    binpool = num2str(binpool,'%023d');
+    binpool = dec2bin(pool， Length);
 end
